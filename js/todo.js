4e639f7fff2511d47bc37c7eb812f21fee847eec
Tyler Jarjoura <cghero764@gmail.com>
2014-11-18 18:09:18 -0500

    rewritten in jQuery

--- js/todo.js
+++ js/todo.js
@@ -1,67 +1,28 @@
-function addTodo()
-{
-	var txt = document.getElementById("input-text").value;
-	if (txt.length == 0)
+function clearList(event) {
+	$("li").remove();
+}
+
+function removeClicked() {
+	$(this).parent().remove();
+}
+
+function addEntry(event) {
+	if (event.which != 13)
 		return;
+	var entry = $("<li></li>");
+	var checkbox = $("<input type=\"checkbox\"></input>");	
+	checkbox.click(removeClicked);
+	checkbox.appendTo(entry);
+	entry.append($("#input-text").val());
 
-	var todo_list = document.getElementById("list");
-	var entry = document.createElement("li");
-	var entry_text = document.createTextNode(txt); 
-	var check_box = document.createElement("input");
-	check_box.type = "checkbox";
-	check_box.onclick = function() {check_box.checked = true; removeChecked();}
-
-	entry.appendChild(check_box);
-	entry.appendChild(entry_text);
-	todo_list.appendChild(entry);
-	document.getElementById("input-text").value = "";
+	entry.appendTo("#list");
+	$("#input-text").val("");
 }
 
-function handleKey(event)
+function main()
 {
-	if (event.which == 13 || event.keyCode == 14) 
-		addTodo();
+	$("#input-text").keypress(addEntry);
+	$("#clear-button").click(clearList);
 }
 
-function clearTodo()
-{
-	var list = document.getElementById("list");
-	var list_entries = list.children;
-
-	if (list_entries.length > 0) {
-		while (list_entries.length > 0) 
-			list.removeChild(list_entries[0]);
-	}
-}
-
-function removeChecked()
-{
-	var list = document.getElementById("list");
-
-	for (var i = 0; i < list.children.length; i++) {	
-		if (list.children[i].children[0].checked) {
-			list.removeChild(list.children[i]);
-			return;
-		}
-	}
-}
-
-function updateTodo()
-{
-	var list = document.getElementById("list");
-	var list_entries = list.children;
-
-	var i = 0;
-	while (i != list_entries.length) {
-		var entry = list_entries[i]; 
-		console.log(entry.children);
-		if (entry.children[0].checked) {
-			list.removeChild(list_entries[i]);
-		} else
-			i++;
-	}	
-}
-
-document.getElementById("submit-button").onclick = addTodo; 
-document.getElementById("input-text").onkeypress = handleKey;
-document.getElementById("clear-button").onclick = clearTodo;
+$(document).ready(main);
